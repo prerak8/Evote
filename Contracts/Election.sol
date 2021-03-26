d10fdfb87c329a4074f0462ae8acf8d45d1c99e9
Prerak Patel <patel170103044@iitg.ac.in>
2021-03-26 16:53:20 +0530

    Election contract updated

--- Contracts/Election.sol
+++ Contracts/Election.sol
@@ -11,6 +11,7 @@ contract Election {
     struct Candidate {
         string name;
         uint256 voteCount;
+        address candidateAddress;
         bool isExist;
     }
     
@@ -18,6 +19,13 @@ contract Election {
     mapping (address => Candidate) public candidateDetails;
     
     address admin;
+    
+    uint startTime;
+    uint regPeriod = 45 seconds;
+    uint votingPeriod = 45 seconds;
+    bool isSorted;
+    address[] public CandidateList;
+    
     
     modifier onlyAdmin {
         require(msg.sender == admin);
@@ -30,22 +38,61 @@ contract Election {
         _;
     }
 
+    modifier isInRegPhase() {
+        require(now <= startTime + regPeriod seconds && now > startTime, "You can't register right now");
+        _;
+    }
+    
+     modifier isInVotingPhase() {
+        require(now <= startTime + regPeriod + votingPeriod && now > startTime + regPeriod seconds, "You can't vote right now");
+        _;
+    }
+    
+    modifier isInResultPhase() {
+        require(now > startTime + regPeriod + votingPeriod, "Results are not yet decleared");
+        _;
+    }
+
     function Election() public {
         admin = msg.sender;
+        startTime = now;
+        isSorted = false;
     }
    
-    function addVoter(address vtrAdd, Voter vtr) onlyAdmin public {
+    function addVoter(address vtrAdd, Voter vtr) onlyAdmin isInRegPhase public {
         voterDetails[vtrAdd] = vtr;
     }
     
-    function addCandidate(address cndAdd, Candidate cnd) onlyAdmin public {
-        candidateDetails[cndAdd] = cnd;
+    function addCandidate(Candidate cnd) onlyAdmin  public {
+        candidateDetails[cnd.candidateAddress] = cnd;
+        CandidateList.push(cnd.candidateAddress);
     }
     
-    function vote(address cndAdd) notVotedBefore(msg.sender) public {
+    function vote(address cndAdd) notVotedBefore(msg.sender) isInVotingPhase public {
         address vtr = msg.sender;
         if (candidateDetails[cndAdd].isExist == false) return ;
         voterDetails[vtr].Voted = true;
         candidateDetails[cndAdd].voteCount++;
+    }
+    
+    
+    function bubbleSort() {
+        for (uint i = 0; i < CandidateList.length; i++) {
+            for (uint j = 0; j + 1 < CandidateList.length; j++) {
+                if (candidateDetails[CandidateList[j]].voteCount < candidateDetails[CandidateList[j+1]].voteCount) {
+                    address temp = CandidateList[j];
+                    CandidateList[j] = CandidateList[j+1];
+                    CandidateList[j+1] = temp;
+                }
+            }
+        }
+    } 
+
+    function declareResult() isInResultPhase returns(address[] memory) {
+        if (isSorted == false) {
+            bubbleSort();
+            isSorted = true;
+        }
+        return CandidateList;
     }
 }
